a0c4fadc1d5948260f887a0350d8c3061d06fb22
alex <alex@alex-HP-15-Notebook-PC>
2018-07-07 16:26:37 -0600

    Fix

--- src/principal/mapas/Mapa.java
+++ src/principal/mapas/Mapa.java
@@ -19,7 +19,7 @@ import principal.sprites.Sprite;
  */
 public class Mapa {
 
-    private String[] partes;
+    private final String[] partes;
 
     private final int ancho;
     private final int alto;
@@ -55,6 +55,7 @@ public class Mapa {
         String spritesEnteros = partes[5];
 
         String[] cadenasSprites = spritesEnteros.split(" ");
+        
 
         sprites = extraerSprites(cadenasSprites);
 
@@ -69,11 +70,16 @@ public class Mapa {
         for (int i = 0; i < partesPaleta.length; i++) {
             String spriteTemporal = partesPaleta[i];
 
-            String[] partesSprite = spriteTemporal.split("\\-");
+            String[] partesSprite = spriteTemporal.split("-");
+            
+          
 
             int indicePaleta = Integer.parseInt(partesSprite[0]);
-            int indiceSpriteHoja = Integer.parseInt(partesSprite[1]);
-
+            
+           
+            int indiceSpriteHoja = Integer.parseInt(partesSprite[2]);
+            System.out.println(paleta.length);
+            
             paleta[indicePaleta] = hoja.getSprites(indiceSpriteHoja);
         }
         return paleta;
