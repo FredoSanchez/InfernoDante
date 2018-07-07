2b0b6e9920fe95b4086316c5dc43a20d10fa1801
alex <alex@alex-HP-15-Notebook-PC>
2018-07-07 09:49:59 -0600

    Mapa

--- src/principal/herramientas/CargadorRecursos.java
+++ src/principal/herramientas/CargadorRecursos.java
@@ -11,7 +11,10 @@ import java.awt.GraphicsEnvironment;
 import java.awt.Image;
 import java.awt.Transparency;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import javax.imageio.ImageIO;
 
 /**
@@ -54,5 +57,36 @@ public class CargadorRecursos {
         g.dispose();
 
         return imagenAcelerada;
+    }
+    
+    public static String leerArchivoTexto(final String ruta){
+        String contenido="";
+        
+        InputStream entradaBytes=ClassLoader.class.getResourceAsStream(ruta);
+        BufferedReader lector = new BufferedReader(new InputStreamReader(entradaBytes));
+        
+        String linea;
+        
+        try{
+            while((linea = lector.readLine()) != null){
+                contenido+= linea;
+            }
+          } catch(IOException e){
+              
+          } finally{
+            
+            try{
+                if(entradaBytes!=null){
+                    entradaBytes.close();
+                }
+                if(lector!=null){
+                    lector.close();
+                }
+            }catch(IOException ex){
+                ex.printStackTrace();
+            }
+            
+        }
+        return contenido;
     }
 }
--- src/principal/maquinaestado/estados/juego/GestorJuego.java
+++ src/principal/maquinaestado/estados/juego/GestorJuego.java
@@ -5,8 +5,10 @@
  */
 package principal.maquinaestado.estados.juego;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import principal.herramientas.CargadorRecursos;
 import principal.maquinaestado.EstadoJuego;
 import principal.sprites.HojaSprites;
 
@@ -14,16 +16,21 @@ import principal.sprites.HojaSprites;
  *
  * @author fredy
  */
-public class GestorJuego implements EstadoJuego{
-    
+public class GestorJuego implements EstadoJuego {
+
     private GestorMapa gestorMapa;
-    
+
+    String texto = CargadorRecursos.leerArchivoTexto("/texto/prueba");
+
     @Override
     public void actualizar() {
     }
 
     @Override
     public void dibujar(Graphics g) {
+        g.setColor(Color.white);
+        g.drawString(texto, 10, 10);
+
     }
-    
+
 }
